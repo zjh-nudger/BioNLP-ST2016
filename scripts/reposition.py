2b8013209a03bab40d6ad50a8b5f0e1c285b5f4a
Jianhai Zhang <jianhai_zhang0411@163.com>
2016-03-08 18:37:56 +0800

    zjh

--- scripts/reposition.py
+++ scripts/reposition.py
@@ -7,12 +7,26 @@ sentence split以及tokenization之后标注重定位
 @author: tanfan.zjh
 """
 
-import glob
+import codecs
 
 d = 'C:/Users/tanfan.zjh/Desktop'
 
-ss_file = open(d+'/test_ss.txt')
-src_file = open(d+'/test_src.txt')
-a1_file = open(d+'/test.a1')
+ss_file = open(d+'/test1_ss.txt')
+src_file = codecs.open(d+'/test1_src.txt','r','utf-8')
+a1_file = open(d+'/test1.a1')
+
+
+txt = ''.join([line for line in src_file])
+ss_txt = ''.join([line for line in ss_file])
+
+span_dic = {}
+for line in a1_file:
+    if ';' in line:
+        continue
+    toks = line.strip().split('\t')
+    toks1 = toks[1].split()
+    start = int(toks1[1])
+    end = int(toks1[2])
+    span_dic[(start,end)] = toks[0]
 
 
